a446bb57195841c273d1187e859437f0b75dc4b6
meeks21 <anthony.meeks3@gmail.com>
2020-05-01 16:30:59 -0700

    creating objects

--- js/assignment_3.js
+++ js/assignment_3.js
@@ -1,28 +1,30 @@
 // creates the object template
-function Guitars (brand, inventory, sold, condition) {
+function Guitars(brand, inventory, sold) {
     this.brand = brand;
     this.inventory = inventory;
-    this.sold =sold;
-    this.condition = ['new','used'];
+    this.sold = sold;
+    // this.condition = ['new','used'];
 
     this.inStocK = function() {
         return this.inventory - this.sold;
     };
 
-    this.quality = function() {
-        return this.condition;
-    }
+    // this.quality = function() {
+    //     return this.condition;
+    // };
 }
 
 // Creates Object instances
-var gibsonlpGuitar = new Guitars('Gibson ' + 'LP', 5, 3, condition[0]);
-var gibsonsgGuitar = new Guitars('Gibson ' + 'SG', 7, 3, condition[0,1]);
-var FenderS = new Guitars('Fender ' + 'Strat', 15, 4, condition[0,1]);
-var FenderJ = new Guitars('Fender ' + 'Jaguar', 2, 1, condition[1]);
-var prs = new Guitars('PRS', 2, 1, condition[0]);
-var ibenez = new Guitars('Ibenez', 4, 1, condition[0]);
+var gibsonlpGuitar = new Guitars('Gibson', 5, 3);
+var gibsonsgGuitar = new Guitars('Gibson', 7, 3);
+// var FenderS = new Guitars('Fender ' + 'Strat', 15, 4, condition[0,1]);
+// var FenderJ = new Guitars('Fender ' + 'Jaguar', 2, 1, condition[1]);
+// var prs = new Guitars('PRS', 2, 1, condition[0]);
+// var ibenez = new Guitars('Ibenez', 4, 1, condition[0]);
 
 // prints to HTML
-var info = gibsonlpGuitar.brand + ' in stock: ';
-    info += gibsonlpGuitar.inStock();
-    info += gibsonlpGuitar.quality();
+var productInfo = gibsonlpGuitar.brand + ' in stock: ';
+    productInfo += gibsonlpGuitar.inStock();
+    // info += gibsonlpGuitar.quality();
+var elguitar1 = document.getElementById('guitar1');
+elguitar1.textContent = productInfo;
