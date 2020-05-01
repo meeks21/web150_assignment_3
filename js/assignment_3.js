9e750851e0e5c4fa96ac60989516090ae52c29bf
meeks21 <anthony.meeks3@gmail.com>
2020-05-01 01:55:42 -0700

    creates multiple objects

--- js/assignment_3.js
+++ js/assignment_3.js
@@ -1,4 +1,4 @@
-
+// creates the object template
 function Guitars (brand, inventory, sold, condition) {
     this.brand = brand;
     this.inventory = inventory;
@@ -7,12 +7,22 @@ function Guitars (brand, inventory, sold, condition) {
 
     this.inStocK = function() {
         return this.inventory - this.sold;
-        return this.condition;
-
     };
+
+    this.quality = function() {
+        return this.condition;
+    }
 }
 
+// Creates Object instances
 var gibsonlpGuitar = new Guitars('Gibson ' + 'LP', 5, 3, condition[0]);
 var gibsonsgGuitar = new Guitars('Gibson ' + 'SG', 7, 3, condition[0,1]);
 var FenderS = new Guitars('Fender ' + 'Strat', 15, 4, condition[0,1]);
-var FenderS = new Guitars('Fender ' + 'Strat', 15, 4, condition[0,1]);
+var FenderJ = new Guitars('Fender ' + 'Jaguar', 2, 1, condition[1]);
+var prs = new Guitars('PRS', 2, 1, condition[0]);
+var ibenez = new Guitars('Ibenez', 4, 1, condition[0]);
+
+// prints to HTML
+var info = gibsonlpGuitar.brand + ' in stock: ';
+    info += gibsonlpGuitar.inStock();
+    info += gibsonlpGuitar.quality();
